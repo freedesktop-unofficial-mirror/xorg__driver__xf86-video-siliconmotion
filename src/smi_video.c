505ad50f45716581753ed2d7642a90e0235d2a64
Kaleb Keithley <kaleb@freedesktop.org>
2004-03-04 20:16:31 +0000

    no bug report, see prior version. Fix bad edit

--- src/smi_video.c
+++ src/smi_video.c
@@ -41,7 +41,7 @@ Author of changes: Corvin Zahn <zahn@zac.de>
 Date:   2.11.2001
 */
 
-: /* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_video.c,v 1.14 2003/12/08 16:03:58 alanh Exp $ */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_video.c,v 1.1.4.2.4.1 2004/03/04 17:47:53 eich Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/siliconmotion/smi_video.c,v 1.13 2003/11/10 18:22:26 tsi Exp $ */
 
 #include "smi.h"
